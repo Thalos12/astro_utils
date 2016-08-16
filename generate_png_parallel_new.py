d9485dc76b3741fb28ab1839f3b6df03a7ef9559
Gio <giovanni.munaretto@hotmail.it>
2016-08-16 12:12:06 +0200

    aggiunti maxmin_new e generate_png_parallel_new

---
+++ generate_png_parallel_new.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import argparse
+import logging
+from multiprocessing import Pool
+import numpy as np
+from mayavi import mlab
+import maxmin
+import math as m
+
+
+# questa funzione serve per usare il codice in parallelo o meno
+def gen_png_wrapper(i, f, res, basename, extension='.dat', parallel=False, use_processes=2, folder='.', header_lines=0):
+    if parallel:
+        f_args = []
+        for k in range(i, f):
+            f_args.append([res, basename+"%05d"%(k,), extension, header_lines])  # creo una lista da dare in pasto al metodo map del Pool
+        # print f_args
+        p = Pool(processes=use_processes)
+        try:
+            output = p.map(gen_png, f_args)
+        except:
+            print 'Something went wrong, please do not use multiprocessing.'
+    else:
+        output = []
+        for k in range(i, f):
+            filename = basename+"%05d"%(k,)
+            output.append(gen_png(res, filename, extension, header_lines))
+    print "Result of the operation:"
+    for o in output:
+        print o
+
+
+# funzione che da in output i file .png
+def gen_png(*args):
+    if len(args) == 1:
+        if len(args[0]) == 4:
+            res = args[0][0]
+            f_name = args[0][1]
+            extension = args[0][2]
+            header_lines = args[0][3]
+    elif len(args) == 4:
+        res = args[0]
+        f_name = args[1]
+        extension = args[2]
+        header_lines = args[3]
+    else:
+        print "Wrong number or type of arguments, aborting"
+        return (f_name, -1)
+
+    global mfig
+    mfig.scene.disable_render = True  # serve per risparmiare un po' di risorse
+
+    data = np.loadtxt(f_name+extension, skiprows=header_lines)
+    header = np.genfromtxt(f_name+extension, max_rows=4)  # salvo le righe di header per t e dt
+
+    global t
+    t += header[-1]
+    dt = header[-1]
+
+    to_delete = []  # il ciclo che segue serve a creare la "fetta vuota" nel plot
+    for i in range(0, data.shape[0]):
+        if (data[i,1] < 0 and data[i,0] >0):
+            to_delete.append(i)
+    data = np.delete(data, to_delete, axis=0)
+    rho = data[::res,-2]
+    logrho=[m.log10(float(i)) for i in rho]
+
+    mfig.scene.disable_render = False
+
+    global dmax
+    global dmin
+    
+    mlab.points3d(data[::res,0], data[::res,1], data[::res,2], logrho, colormap='jet', mode='sphere',scale_mode='none',scale_factor=8, vmax=dmax, vmin=dmin)  # posiziono i punti in 3d
+    mlab.colorbar(title="Density", orientation='horizontal', nb_labels=6)  # faccio apparire la colorbar
+    mlab.text(0.10, 0.90, "time:{}  dt:{}".format(t, dt), figure=mfig, width=0.2)  # aggiungo le scritte con tempo e dt
+
+    if '/' in f_name:  # ottengo il nome della simulazione
+        base = f_name.split('/')[-1]
+    else:
+        base = f_name
+    title = base.split('.')[0]  # ottengo la parte prima di tutti i punti: SIM.column.00xxx -> SIM
+    # print "Title: ", title
+    mlab.text(0.70, 0.90, title, width=0.2)
+
+    mlab.axes(nb_labels=5, x_axis_visibility=False, z_axis_visibility=False,ranges=[-100.0,100.0,-100.0,100.0,-100.0,100.0])
+    outline = mlab.outline()
+    outline.outline_mode = 'cornered'
+    # mlab.axes(y_axis_visibility=False, z_axis_visibility=False)
+    mlab.view(-45.0, 90.0, distance=1000)  # imposto l'angolo di visione dell'insieme dei dati
+    # mlab.show()
+    mlab.savefig(f_name+'.png')
+    mlab.clf(mfig)  # pulisco la figura per prepararla al prossimo set di dati
+
+    return (f_name, 0)
+
+
+# funzione che chiama il generatore di png per tutti i file
+def gen_multiple_data(start_n_file, end_n_file, n_particles, basename, extension=''):
+    for i in range(start_n_file, end_n_file):
+        gen_data(n_particles, basename+"%05d"%(i,)+extension)
+
+
+# genera set di dati casuali in forma array(x, y, z, rho)
+def gen_data(particles, filename):
+    d = np.zeros((particles, 4))
+    for i in range(particles):
+        d[i, 0] = round(np.random.rand()*10, 4)
+        d[i, 1] = round(np.random.rand()*10, 4)
+        d[i, 2] = round(np.random.rand()*10, 4)
+        d[i, 3] = round(np.random.rand()*10, 4)
+    np.savetxt(filename, d, fmt='%.4e')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("start_index", type=int, help="Starting index.")
+    parser.add_argument("end_index", type=int, help="Ending index.")
+    parser.add_argument("resolution", type=int, help="Part of the name that is unchanged.")
+    parser.add_argument("basename", type=str, help="Part of the name that is unchanged.")
+    parser.add_argument("-g", "--generate_data", action="store_true", help="Used to generate random data to test output.")
+    parser.add_argument("-m", "--multiprocessing", action="store_true", help="Uses Python's multiprocessing module to speed up the job.")
+    parser.add_argument("-p", "--processes", type=int, default=2, help="Number of processes used to speed up the job if option \"-m\" has been specified, defaults to 4.")
+    parser.add_argument("-e", "--extension", type=str, default=".dat", help="Extension of the files, defaults to \".dat\".")
+    parser.add_argument("-s", "--skipheaderlines", type=int, default=0, help="Skip the first s lines of the data files.")
+    args = parser.parse_args()
+
+    args.end_index += 1 # serve per fare il png di tutti i file
+
+    if args.generate_data: # genera file con dati random per provare o script
+        gen_multiple_data(start_n_file=args.start_index, end_n_file=args.end_index, n_particles=10000, basename=args.basename, extension=args.extension)
+
+    if not os.path.isfile(args.basename+"%05d"%(args.start_index,)+args.extension): # verifica che ci sia almeno il primo file
+        print "File {} does not exixts.".format(args.basename+"%05d"%(args.start_index,)+args.extension)
+        sys.exit(1)
+    dmax, dmin = maxmin.gen_maxmin_dat(args.start_index, args.end_index, args.basename, args.skipheaderlines, args.resolution) # calcolo massimo e minimo della densità
+    dmax=m.log10(float(dmax))
+    dmin=m.log10(float(dmin))
+    print dmax,dmin
+    print "Finished calculating maximum ({}) and minimum ({}) density.".format(dmax,dmin)
+
+    # sys.exit()
+
+    t = 0 # memorizza il tempo totale della simulazione
+
+    mfig = mlab.figure(size=(800,800)) # inizializzo una figura
+
+    gen_png_wrapper(i=args.start_index, f=args.end_index, res=args.resolution, basename=args.basename,
+                    extension=args.extension, parallel=args.multiprocessing, use_processes=args.processes, header_lines=args.skipheaderlines)
---
+++ maxmin_new.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import argparse
+import os
+
+
+def maxmin(initial, final, basename, sk, res):
+    maxdens,mindens=0,0
+    for i in range(initial,final):
+        path=basename+'%05d'%(i,)
+        data=np.genfromtxt(path,skip_header=sk)
+        density=data[::res,-2]
+        mindens_temp = np.amin(density)
+        maxdens_temp = np.amax(density)
+        if i==initial:
+            maxdens=maxdens_temp
+            mindens=mindens_temp
+        else:
+            if mindens_temp<mindens:
+                mindens=mindens_temp
+            if maxdens_temp>maxdens:
+                maxdens=maxdens_temp
+        print "Iteration n°",i,"of",final-1,"Completed."
+
+    return (maxdens,mindens)
+
+
+# serve per scrivere il file maxmin.dat o, se è già presente, per leggerlo e ritornare i valori
+def gen_maxmin_dat(initial, final, basename, sk, res):
+    if '/' in basename: # questo ciclo serve per prendere il path giusto per il maxmin.dat
+        b = basename.split('/')
+        b[-1] = 'maxmin.dat'
+        n = '/'.join(b)
+    else:
+        n = 'maxmin.dat'
+    if os.path.isfile(n):
+        print "File maxmin.dat found."
+        with open(n,'r') as f:
+            dat = f.readlines()
+        print "Initial: {}Final: {}Basename: {}Resolution: {}Maxdens: {}Mindens: {}".format(dat[0], dat[1], dat[2], dat[3], dat[4], dat[5])
+        gen = raw_input("Generate new maxmin.dat file or use current one? [new/current]\n---> ")
+        if gen == 'current' or gen=='c':
+            with open(n,'r') as f:
+                data = f.readlines()
+            maxdens = data[-2].rstrip()
+            mindens = data[-1].rstrip()
+            return (maxdens, mindens)
+
+    print "Generating a new maxmin.dat"
+    maxdens, mindens = maxmin(initial, final, basename, sk, res)
+    data = [initial, final-1, basename, res, maxdens, mindens]
+    with open(n,'w') as f:
+        for e in data:
+            f.write(str(e)+'\n')
+    return (maxdens,mindens)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('initial', type=int)
+    parser.add_argument('final', type=int)
+    parser.add_argument('basename', type=str)
+    parser.add_argument('skip_header', type=int)
+    parser.add_argument('res', type=int)
+    args = parser.parse_args()
+    maxdens, mindens = maxmin(args.initial, args.final, args.basename, args.skip_header, args.res)
+    print maxdens, mindens
